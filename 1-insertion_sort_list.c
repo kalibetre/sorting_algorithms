bfa3e594fcaa1271883717a3fa339f2b43822588
kalbetre-alx <kalbetre@icloud.com>
2022-10-06 21:12:00 +0300

    insertion sort algo implementation

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algorithm implementation
+ * @list: the doubly linked list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node, *temp_1, *temp_2;
+
+	if (list != NULL && *list != NULL)
+	{
+		node = (*list)->next;
+		while (node != NULL)
+		{
+			temp_1 = node;
+			node = node->next;
+			while (temp_1->prev != NULL && temp_1->prev->n > temp_1->n)
+			{
+				temp_2 = temp_1->prev;
+				temp_2->next = temp_1->next;
+				temp_1->prev = temp_2->prev;
+
+				if (temp_2->prev != NULL)
+					temp_2->prev->next = temp_1;
+				if (temp_1->next != NULL)
+					temp_1->next->prev = temp_2;
+
+				temp_1->next = temp_2;
+				temp_2->prev = temp_1;
+
+				if (temp_1->prev == NULL)
+					*list = temp_1;
+
+				print_list(*list);
+			}
+		}
+	}
+}
