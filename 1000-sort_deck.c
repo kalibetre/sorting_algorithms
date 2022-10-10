11feba25ad2c6f4e827b1e5f88b4da6fe0679294
kalbetre-alx <kalbetre@icloud.com>
2022-10-10 14:10:36 +0300

    fix betty sytle for 1000

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * get_value - Get the value object
+ * get_card_value - Get the value object
  * @name: name of the card
  *
  * Return: the value of the card
@@ -18,8 +18,8 @@ int get_card_value(const char *name)
 		return (12);
 	else if (strcmp(name, "King") == 0)
 		return (13);
-	
-	return atoi(name);
+
+	return (atoi(name));
 }
 
 /**
@@ -27,7 +27,7 @@ int get_card_value(const char *name)
  * @deck_1: the first deck
  * @deck_2: the second deck
  *
- * Return int 
+ * Return: positive if greater, negative if lesser else 0
  */
 int cmp_deck_func(const void *deck_1, const void *deck_2)
 {
@@ -35,12 +35,12 @@ int cmp_deck_func(const void *deck_1, const void *deck_2)
 	deck_node_t *d_2 = *(deck_node_t **)deck_2;
 
 	if (d_1->card->kind != d_2->card->kind)
-		return d_1->card->kind - d_2->card->kind;
+		return (d_1->card->kind - d_2->card->kind);
 	else
-		return get_card_value(d_1->card->value) - get_card_value(d_2->card->value);
+		return (get_card_value(d_1->card->value) - get_card_value(d_2->card->value));
 }
 
-/**s
+/**
  * sort_deck - sorts a deck of cards
  * @deck: the deck of cards
  */
@@ -57,7 +57,7 @@ void sort_deck(deck_node_t **deck)
 	}
 
 	qsort(cards, 52, sizeof(deck_node_t *), cmp_deck_func);
-	
+
 	*deck = cards[0];
 	cards[0]->prev = NULL;
 	for (i = 1; i < 52; i++)
